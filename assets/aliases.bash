18d0594d70bdee375f86a5c5758bcd4f7d0db172
Alan Moran <bonzofenix@gmail.com>
2018-05-09 18:22:39 -0300

    Remove ls coreutil alias

--- assets/aliases.bash
+++ assets/aliases.bash
@@ -14,7 +14,6 @@ alias cls="clear"
 alias vi="vim"
 alias vim="vim -X -O"
 
-alias ls="/usr/local/opt/coreutils/libexec/gnubin/ls --color=auto"
 alias ll="ls -lah"
 
 alias gl="git log -n 20 --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%Creset %an' --abbrev-commit --date=relative"
