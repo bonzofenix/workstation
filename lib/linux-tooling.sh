8209ee04840a0a19b844cc9601443e04c3a03e08
Joe Goller <joe.goller@aon.com>
2019-05-23 14:50:09 +0000

    Adds om and awscli tools for linux
    Signed-off-by: Alan Moran <bonzofenix@gmail.com>

--- lib/linux-tooling.sh
+++ lib/linux-tooling.sh
@@ -7,6 +7,9 @@ if ! grep -q "/usr/local/bin" ~/.bash_profile; then
   echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bash_profile
 fi
 
+wget -q -O - https://raw.githubusercontent.com/starkandwayne/homebrew-cf/master/public.key | sudo apt-key add -
+echo "deb http://apt.starkandwayne.com stable main" | sudo tee /etc/apt/sources.list.d/starkandwayne.list
+sudo apt-get update
 
 # AG
 sudo apt-get install -y silversearcher-ag
@@ -14,12 +17,17 @@ sudo apt-get install -y silversearcher-ag
 # Direnv
 sudo apt-get install -y direnv
 
+# Direnv
+sudo apt-get install -y awscli
+
+# OM
+sudo apt-get install -y om
+
 # Git-duet
 GOVENDOREXPERIMENT=1 go get github.com/git-duet/git-duet/...
 
 # Spruce
-sudo wget -O /usr/local/bin/spruce "https://github.com/geofffranks/spruce/releases/download/v1.18.0/spruce-linux-amd64"
-sudo chmod +x /usr/local/bin/spruce
+sudo apt-get install -y spruce
 
 # Fly
 sudo wget "https://github.com/concourse/concourse/releases/download/v5.0.0/fly-5.0.0-linux-amd64.tgz" && tar xzvf fly-5.0.0-linux-amd64.tgz
