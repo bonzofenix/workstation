c511c901f904efefd711b3ada6195cf74079ea43
Alan Moran <bonzofenix@gmail.com>
2019-11-12 20:10:49 +0000

    Update docker daemon install

--- lib/linux-tooling.sh
+++ lib/linux-tooling.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -x
+
 echo "Adding usr/local/bin to your PATH"
 if ! grep -q "/usr/local/bin" ~/.bash_profile; then
   echo                                      >> ~/.bash_profile
@@ -7,14 +9,28 @@ if ! grep -q "/usr/local/bin" ~/.bash_profile; then
   echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.bash_profile
 fi
 
+sudo apt-get install -y apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+
+# Get google apt keys
 wget -q -O - https://raw.githubusercontent.com/starkandwayne/homebrew-cf/master/public.key | sudo apt-key add -
 echo "deb http://apt.starkandwayne.com stable main" | sudo tee /etc/apt/sources.list.d/starkandwayne.list
 
-sudo apt-get install -y apt-transport-https
-sudo apt-get update
 
+# Get google apt keys
 wget -q -O - https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+
+# Get docker apt keys
+wget -q -O - https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd63] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+
+sudo apt-get update
 
 # AG
 sudo apt-get install -y silversearcher-ag
@@ -43,6 +59,9 @@ GOVENDOREXPERIMENT=1 go get github.com/git-duet/git-duet/...
 # Spruce
 sudo apt-get install -y spruce
 
+# Install docker
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
 # Fly
 sudo wget "https://github.com/concourse/concourse/releases/download/v5.3.0/fly-5.3.0-linux-amd64.tgz" && tar xzvf fly-5.3.0-linux-amd64.tgz
 sudo mv fly /usr/local/bin/fly
@@ -69,3 +88,8 @@ sudo wget "https://releases.hashicorp.com/vault/1.1.1/vault_1.1.1_linux_amd64.zi
 sudo unzip vault_1.1.1_linux_amd64.zip && rm vault_1.1.1_linux_amd64.zip
 sudo mv vault /usr/local/bin/vault
 sudo chmod +x /usr/local/bin/vault
+
+# cf-mgmt-config cli
+sudo wget "https://github.com/pivotalservices/cf-mgmt/releases/download/v1.0.34/cf-mgmt-config-linux"
+sudo mv cf-mgmt-config-linux /usr/local/bin/cf-mgmt-config
+sudo chmod +x /usr/local/bin/cf-mgmt-config
