0922ef448d1f8859c5f0f8ba908f548442e0dc35
Alan Moran <alan.moran1@aon.com>
2020-05-14 15:42:50 +0000

    Installs azure cli in linux

--- lib/linux-tooling.sh
+++ lib/linux-tooling.sh
@@ -100,3 +100,6 @@ sudo chmod +x /usr/local/bin/cf-mgmt-config
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo mount /tmp -o remount,exec
+
+# Install Azure cli
+curl -L https://aka.ms/InstallAzureCli | bash
