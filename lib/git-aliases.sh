bc1ac6ddb300563d8092cd7e210cce1b8b3a1139
Alan Moran <alan.moranrusso@sky.uk>
2020-11-10 12:05:49 +0000

    Modify git di alias

--- lib/git-aliases.sh
+++ lib/git-aliases.sh
@@ -2,7 +2,7 @@ echo
 echo "Setting up Git aliases..."
 git config --global alias.gst git status
 git config --global alias.st status
-git config --global alias.di diff
+git config --global alias.di diff --color-words
 git config --global alias.co checkout
 git config --global alias.ci duet-commit
 git config --global alias.br "branch --sort=-committerdate"
