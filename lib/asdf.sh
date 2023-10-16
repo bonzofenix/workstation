b53d129f2b60e950edc5c1df65964bb7e2e72b7b
Alan Moran <bonzofenix@gmail.com>
2023-10-16 18:19:39 +0200

    Update profile scripts

--- lib/asdf.sh
+++ lib/asdf.sh
@@ -1,7 +1,4 @@
 
-asdf plugin-add kubectl
-asdf install kubectl 1.17.4
-
 echo 'Adding ASDF env vars to path'
 add_to_profile '# Adding ASDF env vars to path' \
                'export ASDFROOT=$HOME/.asdf' \
