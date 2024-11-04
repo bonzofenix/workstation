e7cc22dd11f5f5d1ececbf1d15d10d79f0970843
Alan Moran <bonzofenix@gmail.com>
2024-11-04 10:57:08 +0100

    Added 'us' alias for submodule update and enhanced log aliases in git configuration.

--- lib/git-aliases.sh
+++ lib/git-aliases.sh
@@ -10,6 +10,7 @@ git config --global alias.sta stash
 git config --global alias.llog "log --date=local"
 git config --global alias.flog "log --pretty=fuller --decorate"
 git config --global alias.lg "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+git config --global alias.us "submodule update --init --force --recursive"
 git config --global alias.lol "log --graph --decorate --oneline"
 git config --global alias.lola "log --graph --decorate --oneline --all"
 git config --global alias.blog "log origin/master... --left-right"
