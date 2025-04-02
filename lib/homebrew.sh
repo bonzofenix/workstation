57053ceecb8a1c438b919c44f4df1a877abd85b1
Alan Fernando Moran <bonzofenix@MacBook-Air.local>
2025-04-03 01:41:42 +0200

    "Remove redundant error suppression and unnecessary ownership change in Homebrew script, ensuring a cleaner and more reliable execution process."

--- lib/homebrew.sh
+++ lib/homebrew.sh
@@ -5,12 +5,12 @@ source $SCRIPT_DIR/common.sh
 
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-brew update || :
-brew doctor || :
-sudo chown -R $(whoami) /usr/local/bin
+brew update
+brew doctor
+# sudo chown -R $(whoami) /usr/local/bin
 brew upgrade
 brew cleanup
-brew bundle --file  $SCRIPT_DIR/../assets/work/Brewfile
+brew bundle --file $SCRIPT_DIR/../assets/work/Brewfile
 
 add_to_profile '# Homebrew Path' \
                'path=("opt/homebrew/bin" $path)'
