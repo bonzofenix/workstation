53096d1672f7f962c10a825aa080634abd102616
Alan Moran <bonzofenix@gmail.com>
2025-11-24 12:07:21 +0100

    Add cherry-pick alias to global git configuration

--- lib/git.sh
+++ lib/git.sh
@@ -50,6 +50,7 @@ git config --global alias.st status
 git config --global alias.di diff --color-words
 git config --global alias.co checkout
 git config --global alias.ci 'duet-commit --signoff'
+git config --global alias.cp 'cherry-pick'
 git config --global alias.br "branch --sort=-committerdate"
 git config --global alias.sta stash
 git config --global alias.llog "log --date=local"
