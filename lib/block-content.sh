bf5e16881c85b06d6d6d544750305fded0824838
Alan Moran <bonzofenix@gmail.com>
2026-07-01 10:46:07 +0200

    chore: sync claude settings and unblock instagram

--- lib/block-content.sh
+++ lib/block-content.sh
@@ -6,7 +6,6 @@ source $SCRIPT_DIR/common.sh
 sudo -v
 add_to_host '127.0.0.1 www.lanacion.com.ar'
 add_to_host '127.0.0.1 www.twitter.com'
-add_to_host '127.0.0.1 www.instagram.com'
 add_to_host '127.0.0.1 www.clarin.com'
 add_to_host '127.0.0.1 www.x.com'
 add_to_host '127.0.0.1 x.com'
